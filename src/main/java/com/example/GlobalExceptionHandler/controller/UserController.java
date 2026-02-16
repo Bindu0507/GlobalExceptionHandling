6e6a342bf48b045572816d8b41391ca555cd497f
HIMAIRALA <iralahimabindu@gmail.com>
2026-02-15 21:07:41 -0500

    Committing the GlobalException handling project

---
+++ src/main/java/com/example/GlobalExceptionHandler/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.GlobalExceptionHandler.controller;
+
+import com.example.GlobalExceptionHandler.exception.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/users/{id}")
+    public String getUser(@PathVariable long id){
+        if (id ==0) {
+            throw  new ResourceNotFoundException("Id is 0" + id);
+        }
+        return "User found with id: " + id;
+    }
+}
---
+++ src/main/java/com/example/GlobalExceptionHandler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.GlobalExceptionHandler.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleException(Exception ex)
+    {
+        Map<String,Object> body = new HashMap<>();
+        body.put("time", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND);
+        body.put("error", "NotFound");
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("error", "Internal Server Error");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
