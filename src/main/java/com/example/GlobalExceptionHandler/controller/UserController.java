9359570796847ac29182cd1725d2e9df287edb23
Bindu0507 <iralahimabindu@hotmail.com>
2026-03-16 22:31:19 -0400

    test

--- src/main/java/com/example/GlobalExceptionHandler/controller/UserController.java
+++ src/main/java/com/example/GlobalExceptionHandler/controller/UserController.java
@@ -15,6 +15,6 @@ public class UserController {
         if (id ==0) {
             throw  new ResourceNotFoundException("Id is 0" + id);
         }
-        return "User exists  with User id: " + id;
+        return "User exists tesing github  with User id: " + id;
     }
 }
