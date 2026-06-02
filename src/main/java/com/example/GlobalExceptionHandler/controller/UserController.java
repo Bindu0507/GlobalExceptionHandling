b173a7961ac2ddf9bd76b04f2fc685c3b96b43c3
Bindu0507 <iralahimabindu@hotmail.com>
2026-06-01 22:04:23 -0400

    Update UserController.java

--- src/main/java/com/example/GlobalExceptionHandler/controller/UserController.java
+++ src/main/java/com/example/GlobalExceptionHandler/controller/UserController.java
@@ -15,6 +15,6 @@ public class UserController {
         if (id ==0) {
             throw  new ResourceNotFoundException("Id is 0" + id);
         }
-        return "Testing Github testing on 20260513: " + id;
+        return "Testing Github testing on 20260601 From Github Desktop: " + id;
     }
 }
